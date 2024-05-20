721b275f41b3b839330853dca9804dcede6ae9cf
surajsingh <er.suraj70@gmail.com>
2024-05-20 21:55:53 +0530

    detect cycle and find starting node

---
+++ src/codes/LinkedList/DetectCycle.java
@@ -0,0 +1,43 @@
+package codes.LinkedList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// LeetCode Link : https://leetcode.com/problems/linked-list-cycle/
+public class DetectCycle {
+
+    // Approach 1 : Using HashSet
+    // TC : O(n)
+    // SC : O(n)
+    public boolean hasCycle1(ListNode head) {
+        if (head == null) return false;
+        ListNode curr = head;
+        Set<ListNode> hs = new HashSet<>();
+
+        while (curr != null) {
+            boolean check = hs.add(curr);
+            if (check == false) return true;
+            curr = curr.next;
+        }
+
+        return false;
+    }
+
+    // Approach 2 : Using Slow and Fast Pointers
+    // TC : O(n) -> Linear time
+    // SC : O(1) -> Constant Space
+    public boolean hasCycle2(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/codes/LinkedList/DetectCycleAndFindStartingNode.java
@@ -0,0 +1,23 @@
+package codes.LinkedList;
+import java.util.*;
+
+// LeetCode Link : https://leetcode.com/problems/linked-list-cycle-ii/
+public class DetectCycleAndFindStartingNode {
+
+    // Approach 1 : Using HashSet
+    // TC : O(n)
+    // SC : O(n)
+    public ListNode detectCycle(ListNode head) {
+        ListNode curr = head;
+        Set<ListNode> hs = new HashSet<>();
+
+        while (curr != null) {
+            boolean check = hs.add(curr);
+            if (check == false) return curr;
+            curr = curr.next;
+        }
+
+        return null;
+    }
+
+}
