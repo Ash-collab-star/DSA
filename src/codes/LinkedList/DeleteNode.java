50719f87b719f07d6585c3a14747074b49c2afd0
Ash-collab-star <152736096+Ash-collab-star@users.noreply.github.com>
2024-05-20 16:41:35 +0530

    Answer DeleteNode.java

--- src/codes/LinkedList/DeleteNode.java
+++ src/codes/LinkedList/DeleteNode.java
@@ -1,6 +1,40 @@
 package codes.LinkedList;
 
 // GFG Link : https://www.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
-public class DeleteNode {
+public class DeleteNode(Node head) {
+  Node curr = head;
+    	Node previous=null;
+    	Node future = curr.next;
+    	int count=1;
+    	
+    	if(curr==null){
+    	    return head;
+    	}
+    	
+    	if(x==1){
+    	    head=head.next;
+    	    return head;
+    	    
+    	}
+    	
+        while(count<x && curr!=null){
+            previous=curr;
+            curr=future;
+            future=curr.next;
+            count++;
+           
+        }
+            
+
+        
+        previous.next = future; 
+        curr.next = null; 
+        
+    
+	
+	 return head;
+}
+
+  
 
 }
